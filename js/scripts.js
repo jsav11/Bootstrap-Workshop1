9bde529a5fa8deba44fdf1f60c2d852f9ca3311b
Jessi Musungu <jessi.savannah@gmail.com>
2023-04-01 12:49:48 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ $(function () {
         }
     });
 });
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal('show');
+    });
+
+    $("#loginButton").click(function () {
+        $("#loginModal").modal('show');
+    });
+
+});
+
